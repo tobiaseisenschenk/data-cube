6a936c81b7d614f8202e9f9d7f81be29ceddf61e
tobi <tobias.eisenschenk@teleclinic.com>
2017-05-10 20:39:56 -0500

    Splash screen, Welcome page

--- src/client/app/home/home.module.ts
+++ src/client/app/home/home.module.ts
@@ -3,9 +3,10 @@ import { HomeComponent } from './home.component';
 import { HomeRoutingModule } from './home-routing.module';
 import { SharedModule } from '../shared/shared.module';
 import { UXDataService } from '../shared/services/ux-data.service';
+import { MaterialModule } from '@angular/material';
 
 @NgModule({
-  imports: [HomeRoutingModule, SharedModule],
+  imports: [HomeRoutingModule, SharedModule, MaterialModule],
   declarations: [HomeComponent],
   exports: [HomeComponent],
   providers: [UXDataService]
