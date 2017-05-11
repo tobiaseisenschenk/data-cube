5c1ad0244d5507335816b62988e0a2c71978f53a
tobi <tobias.eisenschenk@teleclinic.com>
2017-05-11 13:50:58 -0500

    Home Screen rework + content

--- src/client/app/home/home.component.ts
+++ src/client/app/home/home.component.ts
@@ -1,6 +1,4 @@
-import { Component, OnInit } from '@angular/core';
-import { UXDataService } from '../shared/services/ux-data.service';
-import { Logger } from 'angular2-logger/core';
+import { Component } from '@angular/core';
 
 /**
  * This class represents the lazy loaded HomeComponent.
@@ -8,8 +6,13 @@ import { Logger } from 'angular2-logger/core';
 @Component({
   moduleId: module.id,
   selector: 'sd-home',
-  templateUrl: 'home.component.html'
+  templateUrl: 'home.component.html',
+  styles: [`
+    .border-top {
+      border-top: 0.1rem solid #E0E0E0;
+    }
+  `]
 })
 export class HomeComponent {
-  constructor(private _logger :Logger) {}
+  constructor() {}
 }
